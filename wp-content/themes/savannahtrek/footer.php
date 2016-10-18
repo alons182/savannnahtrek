0fc6ced90994fdd050366393a7479fb0b9d7550e
Alonso Chavarria Cubero <alons182@hotmail.com>
2016-10-18 17:03:45 -0600

    rent a car footer

--- wp-content/themes/savannahtrek/footer.php
+++ wp-content/themes/savannahtrek/footer.php
@@ -22,6 +22,11 @@
 			<div class="site-info">
 			<span>&copy; 2016 Savannah Trek</span>
 			</div><!-- .site-info -->
+			
+			<div class="rent-a-car">
+			<span>Need a car? <a href="https://toyotarent.com/?agent=SVNT506&amp;code=WebTRACSVNT&amp;partner=SavannahTrek" target="_blank" >Click here</a></span>
+			<a href="https://toyotarent.com/?agent=SVNT506&amp;code=WebTRACSVNT&amp;partner=SavannahTrek" target="_blank" ><img class="alignnone wp-image-938 size-full" src="http://www.savannahtrekcr.com/wp-content/uploads/2016/09/toyota-rent-a-car.jpg" alt="toyota-rent-a-car" width="470" height="157" /></a>
+			</div>
 			<div class="footer-social">
 				<a href="#"><i class="fa fa-facebook"></i></a>
 				<a href="#"><i class="fa fa-twitter"></i></a>
