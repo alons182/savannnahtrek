166d9b043acf3c47f0c5d0db34b23413f8df70a2
Alonso Chavarria <alonso@avotz.com>
2020-08-03 11:37:25 -0600

    fix polilang

--- wp-content/themes/savannahtrek/functions.php
+++ wp-content/themes/savannahtrek/functions.php
@@ -157,22 +157,20 @@ require get_template_directory() . '/inc/jetpack.php';
 
 require get_template_directory() . '/inc/cpt.php';
 
-    // fix polylang language segmentation
-	add_action( 'rest_api_init' , 'polylang_json_api_init' );
-	add_action( 'rest_api_init' ,  'polylangroute'  );
-
-function polylang_json_api_init(){
+function polylang_json_api_init() {
 	global $polylang;
+  
 	$default = pll_default_language();
 	$langs = pll_languages_list();
+  
 	$cur_lang = $_GET['lang'];
+  
 	if (!in_array($cur_lang, $langs)) {
-		$cur_lang = $default;
+	  $cur_lang = $default;
 	}
+  
 	$polylang->curlang = $polylang->model->get_language($cur_lang);
 	$GLOBALS['text_direction'] = $polylang->curlang->is_rtl ? 'rtl' : 'ltr';
-}
-
-function polylang_json_api_languages(){
-	return pll_languages_list();
-}
+  }
+  
+  add_action('rest_api_init', 'polylang_json_api_init');
